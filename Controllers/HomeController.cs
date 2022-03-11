afabf92572fc7c10a22214ca35f70b512c281246
Luis Pineda <luis.adrian@traxretail.com>
2022-03-11 13:56:00 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using JsonDiffPatchDotNet;
+using WebApiTester.Models;
+
+namespace WebApiTester.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Compare(Request request)
+        {
+            var status = false;
+            string diffs = GetDiffs(request, ref status);
+
+            return Json(new { status, msg = diffs });
+        }
+
+        private string GetDiffs(Request request, ref bool status)
+        {
+            var jdp = new JsonDiffPatch();
+            string output;
+            try
+            {
+                status = true;
+                output = jdp.Diff(request.jsonLeft, request.jsonRight);
+                if (string.IsNullOrEmpty(output))
+                {
+                    status = false;
+                    output = "there are not differences";
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                output = ex.Message;
+            }
+            return output;
+        }
+    }
+}
---
+++ Models/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTester.Models
+{
+    public class Request
+    {
+        public string CompileUrlLeft { get; set; }
+        public string CompileUrlRight { get; set; }
+        public string UrlLeft { get; set; }
+        public string UrlRight { get; set; }
+        public string jsonLeft { get; set; }
+        public string jsonRight { get; set; }
+    }
+}
